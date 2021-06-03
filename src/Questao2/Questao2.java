8966f63ed5600f67f55700916c5dad44369fb502
Natasha <natashabusnardo@gmail.com>
2021-06-03 01:20:12 -0300

    Questão 2

---
+++ src/Questao2/Questao2.java
@@ -0,0 +1,105 @@
+package Questao2;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Questao2<V> {
+
+	private String produto;
+	private V id;
+	private double valor;
+	private Date dataFabricacao;
+	private Date dataVencimento;
+
+	public String getProduto() {
+		return produto;
+	}
+
+	public void setProduto(String produto) {
+		this.produto = produto;
+	}
+
+	public V getId() {
+		return id;
+	}
+
+	public void setId(V id) {
+		this.id = id;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public Date getDataFabricacao() {
+		return dataFabricacao;
+	}
+
+	public void setDataFabricacao(String dataFabricacao) {
+		try {
+			this.dataFabricacao = new SimpleDateFormat("dd/MM/yyyy").parse(dataFabricacao);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(String dataVencimento) {
+		try {
+			this.dataVencimento = new SimpleDateFormat("dd/MM/yyyy").parse(dataVencimento);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static Date StrTodate(String pDate) {
+
+		SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+
+		Date dia = null;
+		try {
+
+			dia = df.parse(pDate);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		return dia;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Questao2 [produto=");
+		builder.append(produto);
+		builder.append(", id=");
+		builder.append(id);
+		builder.append(", valor=");
+		builder.append(valor);
+		builder.append(", dataFabricacao=");
+		builder.append(dataFabricacao);
+		builder.append(", dataVencimento=");
+		builder.append(dataVencimento);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	public Questao2(String produto, V id, double valor, String dataFabricacao, String dataVencimento) {
+		this.produto = produto;
+		this.id = id;
+		this.valor = valor;
+		this.dataFabricacao = StrTodate(dataFabricacao);
+		this.dataVencimento = StrTodate(dataVencimento);
+	}
+
+}
---
+++ src/Questao2/main.java
@@ -0,0 +1,17 @@
+package Questao2;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Questao2<Integer> p1 = new Questao2<>("Esmalte", 25, 6.99,"01/12/2020", "01/09/2022");
+		Questao2<String> p2 = new Questao2<>("Acetona", "23", 6.99,"01/12/2020", "01/09/2022");
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		
+
+	}
+
+}
